6442299f28517c17ccfdaee8041f871441f3f5f5
thongnguyen2001 <thong2382001@gmail.com>
2022-02-27 22:38:18 +0700

    Create Account.java
    create new object

---
+++ Assignmant-HE150031/src/java/Model/Account.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class Account {
+    private int userID;
+    private String email;
+    private String password;
+    private String name;
+    private Date dob;
+    private boolean gender;
+    private String phone;
+    private String address;
+    private Majors major;
+    private Role role;
+
+    public Account() {
+    }
+
+    public Account(int userID, String email, String password, String name, Date dob, boolean gender, String phone, String address, Majors major, Role role) {
+        this.userID = userID;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.dob = dob;
+        this.gender = gender;
+        this.phone = phone;
+        this.address = address;
+        this.major = major;
+        this.role = role;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Majors getMajor() {
+        return major;
+    }
+
+    public void setMajor(Majors major) {
+        this.major = major;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
+    
+}
