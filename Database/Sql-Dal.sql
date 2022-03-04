7473576bf1986f3a9d1e6087ca4b45efb267dd53
thongnguyen2001 <thong2382001@gmail.com>
2022-03-04 22:38:55 +0700

    Update Sql-Dal.sql

--- Database/Sql-Dal.sql
+++ Database/Sql-Dal.sql
@@ -1,3 +1,4 @@
+use [project_Check_Attandance]
 -- tra thong tin hoc sinh trong 1 lop
 Select Class.ClassName, [User].UserID, [User].name, [User].email, [User].password, [User].dob,[User].gender, [User].phone, [User].address, [User].majorsID, Majors.MajorsName , [User].roleID, [Role].roleName
 from Class 
@@ -6,3 +7,15 @@ left join [User] on ClassMember.UserID = [User].UserID
 left join Majors on [User].majorsID = [Majors].MajorsID
 left join [Role] on [User].roleID = [Role].roleID
 where Class.ClassID = 2
+
+
+--Tra thoi khoa bieu 
+select ScheID,  [Schedule].UserID, [User].name, [User].email, [User].password, [User].dob,[User].gender, [User].phone, [User].address, 
+[User].majorsID, Majors.MajorsName , [User].roleID, [Role].roleName, Schedule.ClassID, Class.ClassName, [Subject].SubID, [Subject].SubCode,[Subject].Subname, 
+[Subject].numberSlot, Schedule.StartDate, Schedule.EndDate
+from Schedule 
+left join [User] on Schedule.UserID = [User].UserID
+left join Majors on [User].majorsID = [Majors].MajorsID
+left join [Role] on [User].roleID = [Role].roleID
+left join [Subject] on Schedule.SubID = [Subject].SubID
+left join [Class] on Schedule.ClassID = [Class].ClassID where Schedule.UserID = 10
