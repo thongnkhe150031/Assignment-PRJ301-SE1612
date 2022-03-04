55ec21b51f5615c5b9ef3571eddee9f7f8a15020
thongnguyen2001 <thong2382001@gmail.com>
2022-03-05 03:05:28 +0700

    Create BaseAuthen.java

---
+++ Assignmant-HE150031/src/java/Controller/Authentication/BaseAuthen.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.Authentication;
+
+import DAL.AccountDB;
+import Model.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+public abstract class BaseAuthen extends HttpServlet {
+
+    private boolean isAuthenticated(HttpServletRequest request) {
+        Account account = (Account) request.getSession().getAttribute("account");
+        if (account != null) {
+            return true;
+        } else {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                String username = null;
+                String password = null;
+                for (Cookie cooky : cookies) {
+                    if (cooky.getName().equals("username")) {
+                        username = cooky.getValue();
+                    } else if (cooky.getName().equals("password")) {
+                        password = cooky.getValue();
+                    }
+                }
+                AccountDB db = new AccountDB();
+                account = db.getAccount(username, password);
+                if (account != null) {
+                    request.getSession().setAttribute("account", account);
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (isAuthenticated(request)) {
+            processGet(request, response);
+        } else {
+            response.getWriter().println("access denied!");
+        }
+
+    }
+
+    protected abstract void processGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException;
+
+    protected abstract void processPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (isAuthenticated(request)) {
+            processGet(request, response);
+        } else {
+            response.getWriter().println("access denied!");
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
