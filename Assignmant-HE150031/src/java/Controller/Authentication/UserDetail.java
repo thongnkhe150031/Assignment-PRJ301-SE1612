f0cf995a7613bb5c9d2ba96c30b323eb7972579f
thongnguyen2001 <thong2382001@gmail.com>
2022-03-05 03:05:39 +0700

    Update UserDetail.java

--- Assignmant-HE150031/src/java/Controller/Authentication/UserDetail.java
+++ Assignmant-HE150031/src/java/Controller/Authentication/UserDetail.java
@@ -33,60 +33,22 @@ public class UserDetail extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-//        AccountDB db = new AccountDB();
-//        Account account = db.getAccount("thongnkhe150031@fpt.edu.vn", "123456");
-//        if(account == null){
-//            response.getWriter().print("null");
-//        }else{
-//            response.getWriter().print(account.getEmail());
-//        }
-//        ClassDB classDB = new ClassDB();
-//        response.getWriter().print(classDB.getClss().size());
-//        ClassDB db = new ClassDB();
-//        ArrayList<Account> userInClass = db.getUserInClass(1);
-//        response.getWriter().print(userInClass.size());      
 
-        ScheduleDB schDB = new ScheduleDB();  
-        ArrayList<Schedule> sche = schDB.getScheduleByTeacher(10);
-        response.getWriter().print(sche.get(0).getClss().getClassName());
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        Account account = (Account) request.getSession().getAttribute("account");        
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
         return "Short description";
