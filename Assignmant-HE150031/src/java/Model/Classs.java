09732d8f5bff27b18d974fe907de7b3f398edecf
thongnguyen2001 <thong2382001@gmail.com>
2022-03-04 22:38:53 +0700

    Create Classs.java

---
+++ Assignmant-HE150031/src/java/Model/Classs.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Classs {
+    private int classID;
+    private String className;
+
+    public Classs() {
+    }
+
+    public Classs(int classID, String className) {
+        this.classID = classID;
+        this.className = className;
+    }
+
+    public int getClassID() {
+        return classID;
+    }
+
+    public void setClassID(int classID) {
+        this.classID = classID;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    
+    
+}
